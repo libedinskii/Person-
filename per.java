631e275a759df4440c8da92f0c033077fafa88a8
libedinskii <124511733+libedinskii@users.noreply.github.com>
2023-12-22 18:52:33 +0900

    Create per.java

---
+++ per.java
@@ -0,0 +1,24 @@
+public class Person {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+    }
+}
